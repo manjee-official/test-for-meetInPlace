d15b60dcb313253da1d315e277fd9a1abc659517
지운만 <manjee.official@gmail.com>
2021-03-12 18:29:05 +0900

    맵 콜백 추가

--- MeetInPlace/app/src/main/java/com/example/meetinplace/MainActivity.kt
+++ MeetInPlace/app/src/main/java/com/example/meetinplace/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.meetinplace
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMapSdk
-import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
 
-class MainActivity : AppCompatActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-		val fm = supportFragmentManager
-		val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-			?: MapFragment.newInstance().also {
-				fm.beginTransaction().add(R.id.map, it).commit()
-			}
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+    }
+
+	override fun onMapReady(naverMap: NaverMap) {
+		Log.i(TAG, "ready naver map")
+	}
+
+	companion object {
+		val TAG = MainActivity::class.qualifiedName
 	}
 }
