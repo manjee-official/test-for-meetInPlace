7566d0b2bc43d40966472cfa3798c5b52fcef464
지운만 <manjee.official@gmail.com>
2021-03-15 13:48:50 +0900

    GeoCodeModel Data class 추가

---
+++ MeetInPlace/app/src/main/java/com/example/meetinplace/data/model/GeoCodeModel.kt
@@ -0,0 +1,32 @@
+package com.example.meetinplace.data.model
+
+data class GeoCodeData(
+    val status: String,
+    val errorMessage: String,
+    val meta: GeoMeta,
+    val address: GeoAddress
+)
+
+data class GeoMeta(
+    val totalCount: Int,
+    val page: Int,
+    val count: Int
+)
+
+data class GeoAddress(
+    val readAddress: String,
+    val jibunAddress: String,
+    val englishAddress: String,
+    val x: String,
+    val y: String,
+    val distance: Double,
+    val addressElements: List<GeoAddressElements>
+)
+
+data class GeoAddressElements(
+    val type: List<String>,
+    val longName: String,
+    val shortName: String,
+    val code: String
+)
+
