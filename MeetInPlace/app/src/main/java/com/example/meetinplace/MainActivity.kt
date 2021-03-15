9661c67bac4c45ae716772eb2c6bf01220a21d03
지운만 <manjee.official@gmail.com>
2021-03-15 13:49:53 +0900

    맵 옵션 추가

--- MeetInPlace/app/src/main/java/com/example/meetinplace/MainActivity.kt
+++ MeetInPlace/app/src/main/java/com/example/meetinplace/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapOptions
 import com.naver.maps.map.OnMapReadyCallback
 
 
@@ -15,8 +16,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_main)
 
         val fm = supportFragmentManager
+		val options = NaverMapOptions().mapType(NaverMap.MapType.Navi).nightModeEnabled(true).indoorEnabled(true)
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-            ?: MapFragment.newInstance().also {
+            ?: MapFragment.newInstance(options).also {
                 fm.beginTransaction().add(R.id.map, it).commit()
             }
 
@@ -25,9 +27,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
 	@UiThread
     override fun onMapReady(naverMap: NaverMap) {
-		naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true)
-        naverMap.isIndoorEnabled = true
-		naverMap.isNightModeEnabled = true
+
     }
 
     companion object {
