e97180a49f7c1c53f80ef34c1211c4a33d1ab7fc
지운만 <manjee.official@gmail.com>
2021-03-24 13:32:04 +0900

    첫 이미지 재생
    - 기본 동작 소스 메소드 화
    - 첫 이미지 재생

--- MeetInPlace/app/src/main/java/com/example/meetinplace/ui/ImageTestActivity.kt
+++ MeetInPlace/app/src/main/java/com/example/meetinplace/ui/ImageTestActivity.kt
@@ -19,70 +19,85 @@ import kotlinx.coroutines.launch
 
 class ImageTestActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityImageTestBinding
+	private lateinit var binding: ActivityImageTestBinding
 
-    private val image1 = R.drawable.boyoung1
-    private val image2 = R.drawable.boyoung2
-    private val image3 = R.drawable.dahee1
+	private val image1 = R.drawable.boyoung1
+	private val image2 = R.drawable.boyoung2
+	private val image3 = R.drawable.dahee1
 
-    var imageList1 = listOf(image1, image2, image3)
-    var imageList2 = listOf(image1)
-    var imageList3 = listOf(image1, image3)
+	private var imageList1 = listOf(image1, image2, image3)
+	private var imageList2 = listOf(image1)
+	private var imageList3 = listOf(image1, image3)
+	private var imageList = listOf(imageList1, imageList2, imageList3)
 
-    private var progressBarList : ArrayList<View> = ArrayList()
+	private var progressBarList: ArrayList<View> = ArrayList()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this@ImageTestActivity, R.layout.activity_image_test)
+	private var currentGroupIndex = 0
+	private var currentImageIndex = 0
 
-        for (drawable in imageList1) {
-            val v = createProgressView()
-            binding.layoutProgress.addView(v)
-            progressBarList.add(v)
-        }
-        startProgress(progressBarList[0] as ProgressBar)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = DataBindingUtil.setContentView(this@ImageTestActivity, R.layout.activity_image_test)
 
-        binding.viewImage.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-        binding.viewImage.setFactory {
-            val imageView = ImageView(this)
-            imageView.scaleType = ImageView.ScaleType.FIT_CENTER
-            imageView
-        }
-        Glide.with(this).load(imageList1[0]).into(binding.viewImage.currentView as ImageView)
-    }
+		setImageSwitcher()
+		setProgressBar(currentGroupIndex = currentGroupIndex)
+		showImage(currentImageIndex = currentImageIndex)
+	}
 
-    private fun createProgressView(): View {
-        val progressBar =
-            ProgressBar(this@ImageTestActivity, null, android.R.attr.progressBarStyleHorizontal)
-        val layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            1f
-        )
-        layoutParams.marginStart = 8f.dpToPixel()
-        progressBar.layoutParams = layoutParams
-        progressBar.max = 300
-        return progressBar
-    }
+	private fun setImageSwitcher() {
+		binding.viewImage.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+		binding.viewImage.setFactory {
+			val imageView = ImageView(this)
+			imageView.scaleType = ImageView.ScaleType.FIT_CENTER
+			imageView
+		}
+	}
 
-    private fun Float.dpToPixel(): Int {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            this,
-            this@ImageTestActivity.resources.displayMetrics
-        ).toInt()
-    }
+	private fun setProgressBar(currentGroupIndex: Int) {
+		for (drawable in imageList[currentGroupIndex]) {
+			val v = createProgressView()
+			binding.layoutProgress.addView(v)
+			progressBarList.add(v)
+		}
+	}
 
-    private fun startProgress(progressBar: ProgressBar) {
-        GlobalScope.launch() {
-            repeat(300) {
-                delay(10L)
-                progressBar.progress = progressBar.progress + 1
-            }
-        }
-    }
+	private fun showImage(currentImageIndex: Int) {
+		startProgress(progressBarList[currentImageIndex] as ProgressBar)
+		Glide.with(this).load(imageList[currentGroupIndex][currentImageIndex]).into(binding.viewImage.currentView as ImageView)
+	}
 
-    companion object {
+	private fun createProgressView(): View {
+		val progressBar =
+				ProgressBar(this@ImageTestActivity, null, android.R.attr.progressBarStyleHorizontal)
+		val layoutParams = LinearLayout.LayoutParams(
+				ViewGroup.LayoutParams.WRAP_CONTENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT,
+				1f
+		)
+		layoutParams.marginStart = 8f.dpToPixel()
+		progressBar.layoutParams = layoutParams
+		progressBar.max = 300
+		return progressBar
+	}
 
-    }
+	private fun Float.dpToPixel(): Int {
+		return TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP,
+				this,
+				this@ImageTestActivity.resources.displayMetrics
+		).toInt()
+	}
+
+	private fun startProgress(progressBar: ProgressBar) {
+		GlobalScope.launch() {
+			repeat(300) {
+				delay(10L)
+				progressBar.progress = progressBar.progress + 1
+			}
+		}
+	}
+
+	companion object {
+
+	}
 }
